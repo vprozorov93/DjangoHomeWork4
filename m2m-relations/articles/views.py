bd5d82e2cf96d85a3ae5db6646e01d69b21879ce
Vladislav Prozorov <93047063+vprozorov93@users.noreply.github.com>
2022-05-05 19:40:42 +0300

    Update views.py

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -5,7 +5,7 @@ from articles.models import Article, ArticleTag
 
 def articles_list(request):
     template = 'articles/news.html'
-    news = Article.objects.all().order_by('-published_at')
+    news = Article.objects.prefetch_related('scopes', 'scopes__tag').order_by('-published_at')
 
     context = {'object_list': news}
 
